aeadf1dab17e7edc65672fda4a652ad39f3765d3
addison-ch <addisonchen.py@gmail.com>
2024-06-06 09:52:37 -0700

    read andrew log format

--- scripts/parse_tcu_data.py
+++ scripts/parse_tcu_data.py
@@ -19,16 +19,20 @@ def run_script(args):
     filepath = args[0]
     with open(filepath, 'r') as input_file:
         with open('signals.txt', 'w') as output_file:
-            for line in input_file:
-                can_id, can_data = process_message(line)
-                msg = db.get_message_by_frame_id(can_id)
-                data_bytes = bytes.fromhex(can_data)
-                decoded_signals = msg.decode(data_bytes)
-                for signal in decoded_signals:
-                    output_file.write(f'{signal} {decoded_signals[signal]}\n')
+            with open('skipped.txt', 'w') as skipped_file:
+                for line in input_file:
+                    if len(line.strip()) != 26:
+                        skipped_file.write(line)
+                        continue
+                    can_id, can_data = process_message(line)
+                    msg = db.get_message_by_frame_id(can_id)
+                    data_bytes = bytes.fromhex(can_data)
+                    decoded_signals = msg.decode(data_bytes)
+                    for signal in decoded_signals:
+                        output_file.write(f'{signal} {decoded_signals[signal]}\n')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Please input path / name of a file to parse.')
+        print('Usage: python3 parse_tcu_data.py <path_to_file>')
         sys.exit()
     run_script(sys.argv[1:])
